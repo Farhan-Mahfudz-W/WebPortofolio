be8b79d89dd0c1001f9cffad5c32dec5384dcf2f
FarhanMW <farhanmahfudz02@gmail.com>
2025-10-01 18:01:41 +0700

    Update chatbot.js

--- frontend/assets/js/chatbot.js
+++ frontend/assets/js/chatbot.js
@@ -7,7 +7,7 @@ export function setupChatbot() {
     const chatSendBtn = document.getElementById("chat-send-btn");
 
     // ✅ Ganti dengan URL backend kamu di Vercel
-    const API_URL = "https://chatbot-backend.vercel.app/api/predict";
+    const API_URL = "https://backend-eta-ashen-61.vercel.app/";
 
     function displayMessage(sender, message, type) {
         const messageDiv = document.createElement("div");
